a14494f00e587101a10e474721073ea6e7716a96
MajedSaade <majedsaade.2001@gmail.com>
2025-05-23 23:48:22 +0300

    Ollama Git Gemma3 : 1b Deployment

--- App.py
+++ App.py
@@ -23,6 +23,8 @@ logger = logging.getLogger(__name__)
 OLLAMA_API_URL = "http://localhost:11434/api"
 CHECK_INTERVAL = 60  # seconds
 
+"Work ffs"
+
 def check_ollama_status():
     """Check if Ollama service is running and Gemma model is available"""
     try:
